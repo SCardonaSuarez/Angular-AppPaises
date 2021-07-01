b025d058ee9c6aa25f165199a0967314de42e8c1
Santo <santicardonas777@gmail.com>
2021-07-01 15:24:42 -0500

    Se termino la base de la aplicacion

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {switchMap} from 'rxjs/operators';
+import {switchMap, tap} from 'rxjs/operators';
+
+import { Country } from '../../interfaces/pais-interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,6 +13,8 @@ import { PaisService } from '../../services/pais.service';
 })
 export class VerPaisComponent implements OnInit {
 
+  pais!:Country
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private paisService:PaisService ) { }
@@ -19,12 +23,10 @@ export class VerPaisComponent implements OnInit {
 
     this.activatedRoute.params
       .pipe(
-        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id))
+        switchMap( ({id}) => this.paisService.getPaisPorAlpha(id)),
+        tap(console.log)
       )
-      .subscribe(resp => {
-        console.log(resp);
-        
-      })
+      .subscribe(pais => this.pais = pais)
 
     // this.activatedRoute.params
     //   .subscribe(({ id }) =>{
@@ -50,6 +52,8 @@ export class VerPaisComponent implements OnInit {
 
   112. Traimos switchMap de rxjs que resive un observable y resgresa un observable
     import {switchMap} from 'rxjs/operators';
+
+  113. Tap
 
 
 
